1d521e9ab9549dd6ba97c4aff6a885a64aa48b06
SkSamuel157 <samuelscs12senai@gmail.com>
2024-04-17 16:40:05 -0300

    Initial commit

---
+++ Ativ03/Program.cs
@@ -0,0 +1,101 @@
+﻿//1 - Joaquim trabalha em uma empresa e recebe R$35.00 reais por hora e trabalha 8 horas por dia. Ele solicitou um programa que calcule quanto ao final de um dia ele deve receber 
+/* using System;
+
+int dinheiro = 35;
+int horas = 8;
+
+int salario = dinheiro * horas;
+
+Console.WriteLine("Seu salário é :" + salario); */
+
+
+
+
+//2 - Cada caixa de bombom vem 27 unidades, Pamela comprou 16 unidades da caixa de bombom, quantos bombons ela tem?
+/*using System;
+
+int numero1 = 27;
+int numero2 = 16;
+
+int subtracao = numero1 - numero2;
+Console.WriteLine("Ela tem: " + subtracao + " Bombons");
+*/
+
+
+
+
+//3 - Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números.
+/*
+using System;
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero01 = 12;
+            int numero02 = 10;
+
+            Console.WriteLine("Seus Numeros são: " + numero01 + " e " + numero02);
+
+            int soma = numero01 + numero02;
+
+            Console.WriteLine("Seu total é: " + soma);
+        }
+    }
+}
+*/
+
+
+
+//4 - Uma loja vendeu 300 garrafas de água, cada caixa vem 20 garrafas, quantas caixas eles usaram?
+/*using System;
+
+namespace PrimeiroProjeto
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 300;
+            int numero2 = 20;
+
+
+            int divisao = numero1 / numero2;
+
+
+            Console.WriteLine("essa é a quantidade de caixas utilizadas: " + divisao);
+        }
+    }
+}*/
+
+
+//5 - É Grande demais
+/*using System;
+
+namespace CalculadoraTermal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double precoInstalacao = 300.00;
+            double precoLimpeza = 150.00;
+            double precoManutencao = 499.99;
+
+
+            double descontoNovoCliente = 50.00;
+
+
+            double totalServicos = precoInstalacao + precoLimpeza + precoManutencao;
+
+
+            double totalAPagar = totalServicos - descontoNovoCliente;
+
+
+            Console.WriteLine("O total a ser pago pela empresa TERMAL é: R$" + totalAPagar);
+        }
+    }
+}*/
